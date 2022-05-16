507256ac05a3906133c786c57a4716eb04058fca
pop-ioan-30123 <99677256+pop-ioan-30123@users.noreply.github.com>
2022-05-16 18:17:45 +0300

    Delete isp-lab-11-12-master/isp-lab-11-12-master directory

--- isp-lab-11-12-master/isp-lab-11-12-master/c1-samples/src/main/java/aut/utcluj/isp/ex4/NegativeAltitudeException.java
+++
@@ -1,4 +0,0 @@
-package aut.utcluj.isp.ex4;
-
-public class NegativeAltitudeException extends RuntimeException {
-}
--- isp-lab-11-12-master/isp-lab-11-12-master/c5-samples/src/main/java/aut/utcluj/isp/ex4/NoTicketAvailableException.java
+++
@@ -1,7 +0,0 @@
-package aut.utcluj.isp.ex4;
-
-/**
- * @author stefan
- */
-public class NoTicketAvailableException extends RuntimeException {
-}
